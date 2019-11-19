1e15fbc1a5292ececc81026dc79c78a5118a8a51
sn_baby <sn_baby@qq.com>
2019-11-20 00:39:03 +0800

    西藏

--- rmzfzc/rmzfzc/spiders/xizang.py
+++ rmzfzc/rmzfzc/spiders/xizang.py
@@ -1,11 +1,116 @@
 # -*- coding: utf-8 -*-
 import scrapy
 
+import logging
+
+from scrapy_splash import SplashRequest
+from rmzfzc.items import rmzfzcItem
+
+script = """
+function main(splash, args)
+  assert(splash:go(args.url))
+  assert(splash:wait(1))
+  return {
+    html = splash:html(),
+  }
+end
+"""
 
 class XizangSpider(scrapy.Spider):
     name = 'xizang'
-    allowed_domains = ['http://www.xizang.gov.cn/']
-    start_urls = ['http://http://www.xizang.gov.cn//']
+
+    custom_settings = {
+        'SPIDER_MIDDLEWARES': {
+            'scrapy_splash.SplashDeduplicateArgsMiddleware': 100,
+        },
+        'DOWNLOADER_MIDDLEWARES': {
+            'scrapy.downloadermiddleware.useragent.UserAgentMiddleware': None,
+            'utils.middlewares.MyUserAgentMiddleware.MyUserAgentMiddleware': 126,
+            'utils.middlewares.DeduplicateMiddleware.DeduplicateMiddleware': 130,
+        },
+        'ITEM_PIPELINES': {
+            'utils.pipelines.MysqlTwistedPipeline.MysqlTwistedPipeline': 64,
+            'utils.pipelines.DuplicatesPipeline.DuplicatesPipeline': 100,
+        },
+        'DUPEFILTER_CLASS': 'scrapy_splash.SplashAwareDupeFilter',
+        'HTTPCACHE_STORAGE': 'scrapy_splash.SplashAwareFSCacheStorage',
+        'SPLASH_URL': 'http://localhost:8050/'}
+
+    def __init__(self, pagenum=None, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.add_pagenum = pagenum
+
+    def start_requests(self):
+        try:
+            urls = [
+                'http://www.xizang.gov.cn/zwgk/xxfb/fgwj/index.html',
+                'http://www.xizang.gov.cn/zwgk/xxfb/zcjd_435/index.html',
+            ]
+            for url in urls:
+                yield SplashRequest(url, args={'lua_source': script, 'wait': 1}, callback=self.parse_page)
+        except Exception as e:
+            logging.error(self.name + ": " + e.__str__())
+            logging.exception(e)
+
+    def parse_page(self, response):
+        page_count = int(self.parse_pagenum(response))
+        try:
+            for pagenum in range(page_count):
+                if pagenum == 0:
+                    url = response.request.url
+                else:
+                    url = response.request.url.replace(
+                        '.html', '_'+str(pagenum) + ".html")
+
+                yield SplashRequest(url, args={'lua_source': script, 'wait': 1}, callback=self.parse)
+        except Exception as e:
+            logging.error(self.name + ": " + e.__str__())
+            logging.exception(e)
+
+    def parse_pagenum(self, response):
+        try:
+            # 在解析页码的方法中判断是否增量爬取并设定爬取列表页数，如果运行
+            # 脚本时没有传入参数pagenum指定爬取前几页列表页，则全量爬取
+            if not self.add_pagenum:
+                return int(response.xpath('//div[@class="pg_num"]').re('([1-9]\d*\.?\d*)')[-1])
+            return self.add_pagenum
+        except Exception as e:
+            logging.error(self.name + ": " + e.__str__())
+            logging.exception(e)
 
     def parse(self, response):
-        pass
+        for href in response.css('div.zwyw_con.lf ul li a::attr(href)').extract():
+            try:
+                yield scrapy.Request(response.request.url.replace('index.html',href),
+                                     callback=self.parse_item, dont_filter=True)
+            except Exception as e:
+                logging.error(self.name + ": " + e.__str__())
+                logging.exception(e)
+        # 处理翻页
+        # 1. 获取翻页链接
+        # 2. yield scrapy.Request(第二页链接, callback=self.parse, dont_filter=True)\
+
+    def parse_item(self, response):
+        try:
+            item = rmzfzcItem()
+            item['title'] = response.css('div.title h1::text').extract_first()
+            item['article_num'] = ''
+            item['content'] = response.css('#news_content').extract_first()
+            item['appendix'] = ''
+            item['source'] = response.css('div.title div.t_left span:nth-child(2)::text').extract_first().replace('来源：','')
+            item['time'] = response.css('div.title div.t_left span:nth-child(1)::text').extract_first().replace('时间：','')
+            item['province'] = ''
+            item['city'] = ''
+            item['area'] = ''
+            item['website'] = '新疆维吾尔自治区人民政府'
+            item['link'] = response.request.url
+            item['txt'] = ''.join(response.css('#news_content *::text').extract())
+            item['appendix_name'] = ''
+            item['module_name'] = '新疆维吾尔自治区人民政府'
+            item['spider_name'] = 'xinjiang'
+            print("===========================>crawled one item" + response.request.url)
+        except Exception as e:
+            logging.error(self.name + " in parse_item: url=" + response.request.url + ", exception=" + e.__str__())
+            logging.exception(e)
+        yield item
+
