0317ac653b45829a9cfb2ff5f701bb16c09198e3
Li Yi <lovelylich@gmail.com>
2019-12-01 19:57:35 +0800

    enable log

--- zfcgw/zfcgw/settings.py
+++ zfcgw/zfcgw/settings.py
@@ -94,9 +94,9 @@ AUTOTHROTTLE_ENABLED = True
 # HTTPCACHE_IGNORE_HTTP_CODES = []
 # HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
 
-LOG_ENABLED = True
-LOG_FILE = "spiders.log"
-LOG_LEVEL = "ERROR"
+#LOG_ENABLED = True
+#LOG_FILE = "spiders.log"
+LOG_LEVEL = "DEBUG"
 
 
 # MYSQL_HOST = '10.8.32.125'
