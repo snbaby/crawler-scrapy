ea7ffd9fb3a7e83660abd34bb0adadf5a3efbd99
liuyi <liuyi@seadun.com>
2019-12-05 20:59:49 +0800

    修改湖北

--- ggjypt/ggjypt/spiders/hubei.py
+++ ggjypt/ggjypt/spiders/hubei.py
@@ -56,7 +56,7 @@ class hubeiSzfwjSpider(scrapy.Spider):
                 }
             ]
             for content in contents:
-                yield SplashRequest(content['url'], args={'lua_source': script, 'wait': 1}, callback=self.parse_page,
+                yield SplashRequest(content['url'], args={'lua_source': script, 'wait': 1}, callback=self.parse_type,
                                     cb_kwargs=content)
         except Exception as e:
             logging.error(self.name + ": " + e.__str__())
@@ -64,51 +64,136 @@ class hubeiSzfwjSpider(scrapy.Spider):
 
     def parse_type(self, response, **kwargs):
         if kwargs['topic'] == 'dzzb':
-            for href in response.xpath('//ul[@class="menu_body"]/li/@onclick'):
+            for href in response.xpath('//*[@class="ewb-sub-tree"]/li/a/@href'):
                 try:
-                    url = href.extract()
-                    url = url.replace('window.location=\'', '').replace('\'', '')
+                    url = response.urljoin(href.extract())
                     print(url)
                     yield SplashRequest(url, callback=self.parse_page, dont_filter=True, cb_kwargs={'url': url})
 
                 except Exception as e:
                     logging.error(self.name + ": " + e.__str__())
                     logging.exception(e)
-        elif kwargs['topic'] == 'dzzb':
-            
-
+        elif kwargs['topic'] == 'jzcg':
+            print(kwargs['url'])
+            yield SplashRequest(kwargs['url'], callback=self.parse_page1, dont_filter=True, cb_kwargs={'url': kwargs['url']})
+        elif kwargs['topic'] == 'pmjy':
+            print(kwargs['url'])
+            yield SplashRequest(kwargs['url'], callback=self.parse_page2, dont_filter=True,
+                                cb_kwargs={'url': kwargs['url']})
 
     def parse_page(self, response,**kwargs):
-        page_count = int(self.parse_pagenum(response))
+        print(response)
+        page_count = int(self.parse_pagenum(response, '0'))
+        print('page_count' + str(page_count))
         try:
             for pagenum in range(page_count):
-                temUrl = kwargs['url'].replace('.jhtml', '')+'_'
-                url = temUrl + \
-                      str(pagenum) + ".jhtml" if pagenum > 0 else kwargs['url']
-                yield SplashRequest(url, args={'lua_source': script, 'wait': 1}, callback=self.parse, dont_filter=True)
+                if pagenum > 0:
+                    temUrl = kwargs['url'].replace('moreinfo_jyxx.html', '')
+                    url = temUrl + \
+                          str(pagenum) + ".html" if pagenum > 1 else kwargs['url']
+                    yield SplashRequest(url, args={'lua_source': script, 'wait': 1}, callback=self.parse, dont_filter=True)
         except Exception as e:
             logging.error(self.name + ": " + e.__str__())
             logging.exception(e)
 
-    def parse_pagenum(self, response):
+    def parse_page1(self, response, **kwargs):
+        print(response)
+        page_count = int(self.parse_pagenum(response, '1'))
+        print('page_count' + str(page_count))
+        try:
+            for pagenum in range(page_count):
+                if pagenum > 0:
+                    temUrl = 'http://www.hbyxjzcg.cn/drug/0-'
+                    url = temUrl + \
+                          str(pagenum) + ".html" if pagenum > 1 else kwargs['url']
+                    yield SplashRequest(url, args={'lua_source': script, 'wait': 1}, callback=self.parse1,
+                                        dont_filter=True)
+        except Exception as e:
+            logging.error(self.name + ": " + e.__str__())
+            logging.exception(e)
+    def parse_page2(self, response, **kwargs):
+        print(response)
+        page_count = int(self.parse_pagenum(response, '2'))
+        print('page_count' + str(page_count))
+        try:
+            for pagenum in range(page_count):
+                if pagenum > 0:
+                    temUrl = kwargs['url'].replace('moreinfo_jyxx.html', '')
+                    url = temUrl + \
+                          str(pagenum) + ".html" if pagenum > 1 else kwargs['url']
+                    yield SplashRequest(url, args={'lua_source': script, 'wait': 1}, callback=self.parse2,
+                                        dont_filter=True)
+        except Exception as e:
+            logging.error(self.name + ": " + e.__str__())
+            logging.exception(e)
+
+    def parse_pagenum(self, response, type):
         try:
             # 在解析页码的方法中判断是否增量爬取并设定爬取列表页数，如果运行
             # 脚本时没有传入参数pagenum指定爬取前几页列表页，则全量爬取
-            size = len(response.xpath('//div[@class="pages"]/ul/li[1]').re(r'([1-9]\d*\.?\d*)'))
-            return int(response.xpath('//div[@class="pages"]/ul/li[1]').re(r'([1-9]\d*\.?\d*)')[size-1])
+            if type == '0':
+                if response.xpath('//*[@class="ewb-page-li ewb-page-noborder ewb-page-num"]/span'):
+                    return int(response.xpath('//*[@class="ewb-page-li ewb-page-noborder ewb-page-num"]/span').re(
+                        r'([1-9]\d*\.?\d*)')[1])
+                else:
+                    return '0'
+            elif type == '1':
+                if response.xpath('//*[@class="flickr"]/a[4]/text()'):
+                    return int(response.xpath('//*[@class="flickr"]/a[4]/text()').extract_first())
+                else:
+                    return '0'
+            elif type == '2':
+                if response.xpath('//*[@class="ewb-page-li ewb-page-noborder ewb-page-num"]/span'):
+                    return int(response.xpath('//*[@class="ewb-page-li ewb-page-noborder ewb-page-num"]/span').re(
+                        r'([1-9]\d*\.?\d*)')[1])
+                else:
+                    return '0'
+
         except Exception as e:
             logging.error(self.name + ": " + e.__str__())
             logging.exception(e)
 
     def parse(self, response):
-        for selector in response.xpath('//ul[@class="article-list2"]/li/div'):
+        for selector in response.xpath('//ul[@class="ewb-news-items"]/li'):
             try:
                 item = {}
                 item['title'] = "".join(selector.xpath('./a//text()').extract())
-                item['time'] = selector.xpath('./div/text()').extract_first()
+                item['time'] = selector.xpath('./span/text()').extract_first().strip()
                 url = selector.xpath('./a/@href').extract_first()
                 print('url=============='+url)
-                yield scrapy.Request(url,callback=self.parse_item, dont_filter=True, cb_kwargs=item)
+                yield scrapy.Request(response.urljoin(url),callback=self.parse_item, dont_filter=True, cb_kwargs=item)
+            except Exception as e:
+                logging.error(self.name + ": " + e.__str__())
+                logging.exception(e)
+        # 处理翻页
+        # 1. 获取翻页链接
+        # 2. yield scrapy.Request(第二页链接, callback=self.parse, dont_filter=True)
+
+    def parse1(self, response):
+        for selector in response.xpath('//ul[@class="ewb-news-items"]/li'):
+            try:
+                item = {}
+                item['title'] = "".join(selector.xpath('./a//text()').extract())
+                item['time'] = selector.xpath('./span/text()').extract_first().strip()
+                url = selector.xpath('./a/@href').extract_first()
+                print('url=============='+url)
+                yield scrapy.Request(response.urljoin(url),callback=self.parse_item, dont_filter=True, cb_kwargs=item)
+            except Exception as e:
+                logging.error(self.name + ": " + e.__str__())
+                logging.exception(e)
+        # 处理翻页
+        # 1. 获取翻页链接
+        # 2. yield scrapy.Request(第二页链接, callback=self.parse, dont_filter=True)
+
+    def parse2(self, response):
+        for selector in response.xpath('//ul[@class="ewb-news-items"]/li'):
+            try:
+                item = {}
+                item['title'] = "".join(selector.xpath('./a//text()').extract())
+                item['time'] = selector.xpath('./span/text()').extract_first().strip()
+                url = selector.xpath('./a/@href').extract_first()
+                print('url=============='+url)
+                yield scrapy.Request(response.urljoin(url),callback=self.parse_item, dont_filter=True, cb_kwargs=item)
             except Exception as e:
                 logging.error(self.name + ": " + e.__str__())
                 logging.exception(e)
@@ -133,7 +218,7 @@ class hubeiSzfwjSpider(scrapy.Spider):
                     category = '单一'
                 item = ztbkItem()
                 item['title'] = title
-                item['content'] = "".join(response.xpath('//div[@class="div-article2"]').extract())
+                item['content'] = "".join(response.xpath('//div[@class="news-article-para"]').extract())
                 item['source'] = response.xpath('//a[@class="originUrl"]/text()').extract_first()
                 item['category'] = category
                 item['type'] = ''
@@ -142,10 +227,10 @@ class hubeiSzfwjSpider(scrapy.Spider):
                 item['website'] = '湖北省公共资源交易服务平台'
                 item['module_name'] = '湖北省-公共交易平台'
                 item['spider_name'] = 'hubei_ggjypt'
-                item['txt'] = "".join(response.xpath('//div[@class="div-article2"]//text()').extract())
-                item['appendix_name'] = ";".join(response.xpath('//div[@class="div-article2"]//a[contains(@href,"pdf") and contains(@href,"doc") and contains(@href,"docx") and contains(@href,"xls")]/text()').extract())
+                item['txt'] = "".join(response.xpath('//div[@class="news-article-para"]//text()').extract())
+                item['appendix_name'] = ";".join(response.xpath('//div[@class="news-article-para"]//a[contains(@href,"pdf") and contains(@href,"doc") and contains(@href,"docx") and contains(@href,"xls")]/text()').extract())
                 item['link'] = response.request.url
-                item['appendix'] = ";".join(response.xpath('//div[@class="div-article2"]//a[contains(@href,"pdf") and contains(@href,"doc") and contains(@href,"docx") and contains(@href,"xls")]/@href').extract())
+                item['appendix'] = ";".join(response.xpath('//div[@class="news-article-para"]//a[contains(@href,"pdf") and contains(@href,"doc") and contains(@href,"docx") and contains(@href,"xls")]/@href').extract())
                 print(
                     "===========================>crawled one item" +
                     response.request.url)
