c464c7344a958b2999c84eb8f05ea3decae0c3f9
sn_baby <sn_baby@qq.com>
2019-12-05 23:25:19 +0800

    修改

---
+++ ggjypt/ggjypt/spiders/gansu.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+import scrapy
+import logging
+
+from scrapy_splash import SplashRequest
+from ggjypt.items import ztbkItem
+
+script = """
+function main(splash, args)
+    splash:go("https://www.baidu.com/")
+    splash:wait(1)
+    return splash:html()
+    end
+"""
+
+class GansuSpider(scrapy.Spider):
+    name = 'gansu'
+    custom_settings = {
+        'SPIDER_MIDDLEWARES': {
+            'scrapy_splash.SplashDeduplicateArgsMiddleware': 100,
+        },
+        'DOWNLOADER_MIDDLEWARES': {
+            'scrapy.downloadermiddleware.useragent.UserAgentMiddleware': None,
+            'utils.middlewares.MyUserAgentMiddleware.MyUserAgentMiddleware': 126,
+            'utils.middlewares.DeduplicateMiddleware.DeduplicateMiddleware': 130,
+        },
+        'ITEM_PIPELINES': {
+            'utils.pipelines.MysqlTwistedPipeline.MysqlTwistedPipeline': 64,
+            'utils.pipelines.DuplicatesPipeline.DuplicatesPipeline': 100,
+        },
+        'DUPEFILTER_CLASS': 'scrapy_splash.SplashAwareDupeFilter',
+        'HTTPCACHE_STORAGE': 'scrapy_splash.SplashAwareFSCacheStorage',
+        'SPLASH_URL': "http://47.106.239.73:8050/"}
+
+    def __init__(self, pagenum=None, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.add_pagenum = pagenum
+
+    def start_requests(self):
+        try:
+            contents = [
+                {
+                    'topic': 'jyxxlb',  # 交易信息列表
+                    'url': 'http://ggzyjy.gansu.gov.cn/f/newprovince/annogoods/list'
+                }
+            ]
+            for content in contents:
+                yield SplashRequest(content['url'],endpoint = 'execute', args={'lua_source': script, 'wait': 1,'endpoint':'execute'}, callback=self.parse,
+                                    cb_kwargs=content)
+        except Exception as e:
+            logging.error(self.name + ": " + e.__str__())
+            logging.exception(e)
+
+    def parse(self, response,**kwargs):
+        print('---------')
+        print(response.text)
+        pass
