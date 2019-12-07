81dc1b89c31206ef7479ba512db6fd2aebc517c2
sn_baby <sn_baby@qq.com>
2019-12-07 11:06:31 +0800

    修改

---
+++ ggjypt/ggjypt/spiders/gansu_ggjypt.py
@@ -0,0 +1,227 @@
+# -*- coding: utf-8 -*-
+import scrapy
+import logging
+
+from scrapy_splash import SplashRequest
+from ggjypt.items import ztbkItem
+
+class GansuGgjyptSpider(scrapy.Spider):
+    name = 'gansu_ggjypt'
+    custom_settings = {
+        'DOWNLOADER_MIDDLEWARES': {
+            'scrapy_splash.SplashCookiesMiddleware': 723,
+            'scrapy_splash.SplashMiddleware': 725,
+            'scrapy.downloadermiddlewares.httpcompression.HttpCompressionMiddleware': 810,
+        },
+        'SPIDER_MIDDLEWARES': {
+            'scrapy_splash.SplashDeduplicateArgsMiddleware': 100,
+        },
+        'ITEM_PIPELINES': {
+            'utils.pipelines.MysqlTwistedPipeline.MysqlTwistedPipeline': 64,
+            'utils.pipelines.DuplicatesPipeline.DuplicatesPipeline': 100,
+        },
+        'DUPEFILTER_CLASS': 'scrapy_splash.SplashAwareDupeFilter',
+        'HTTPCACHE_STORAGE': 'scrapy_splash.SplashAwareFSCacheStorage',
+        'SPLASH_URL': "http://47.106.239.73:8050/"}
+
+    def __init__(self, pagenum=None, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.add_pagenum = pagenum
+
+    def start_requests(self):
+        script = """
+                    function wait_for_element(splash, css, maxwait)
+                      -- Wait until a selector matches an element
+                      -- in the page. Return an error if waited more
+                      -- than maxwait seconds.
+                      if maxwait == nil then
+                          maxwait = 10
+                      end
+                      return splash:wait_for_resume(string.format([[
+                        function main(splash) {
+                          var selector = '%s';
+                          var maxwait = %s;
+                          var end = Date.now() + maxwait*1000;
+
+                          function check() {
+                            if(document.querySelector(selector)) {
+                              splash.resume('Element found');
+                            } else if(Date.now() >= end) {
+                              var err = 'Timeout waiting for element';
+                              splash.error(err + " " + selector);
+                            } else {
+                              setTimeout(check, 200);
+                            }
+                          }
+                          check();
+                        }
+                      ]], css, maxwait))
+                    end
+                    function main(splash, args)
+                      splash:go(args.url)
+                      return splash:html()
+                    end
+                    """
+        try:
+            contents = [
+                {
+                    'topic': 'sjjyxx',  # 省局交易信息
+                    'url': 'http://ggzyjy.gansu.gov.cn/f/newprovince/annogoods/list'
+                }
+            ]
+            for content in contents:
+                yield SplashRequest(content['url'],
+                                    endpoint='execute',
+                                    args={
+                                        'lua_source': script,
+                                        'wait': 1,
+                                        'url': content['url'],
+                                    },
+                                    callback=self.parse_page,
+                                    cb_kwargs=content)
+        except Exception as e:
+            logging.error(self.name + ": " + e.__str__())
+            logging.exception(e)
+
+    def parse_page(self, response, **kwargs):
+        script = """
+            function wait_for_element(splash, css, maxwait)
+              -- Wait until a selector matches an element
+              -- in the page. Return an error if waited more
+              -- than maxwait seconds.
+              if maxwait == nil then
+                  maxwait = 10
+              end
+              return splash:wait_for_resume(string.format([[
+                function main(splash) {
+                  var selector = '%s';
+                  var maxwait = %s;
+                  var end = Date.now() + maxwait*1000;
+
+                  function check() {
+                    if(document.querySelector(selector)) {
+                      splash.resume('Element found');
+                    } else if(Date.now() >= end) {
+                      var err = 'Timeout waiting for element';
+                      splash.error(err + " " + selector);
+                    } else {
+                      setTimeout(check, 200);
+                    }
+                  }
+                  check();
+                }
+              ]], css, maxwait))
+            end
+            function main(splash, args)
+
+              assert(splash:go(args.url))
+              wait_for_element(splash, ".active")
+              wait_for_element(splash, ".sTradingInformationSelectedBtoList a")
+              splash:runjs("document.querySelector('.sTradingInformationSelectedBtoList').innerHTML = ''")
+              splash:runjs("document.querySelector('.active').classList.remove('active')")
+              js = string.format("page(%d,10,'')", args.pagenum)
+              splash:evaljs(js)
+              wait_for_element(splash, ".active")
+              wait_for_element(splash, ".sTradingInformationSelectedBtoList a")
+              return splash:html()
+            end
+            """
+        page_count = int(self.parse_pagenum(response, kwargs))
+        try:
+            for pagenum in range(page_count):
+                url = kwargs['url']
+                yield SplashRequest(url,
+                                    endpoint='execute',
+                                    args={
+                                        'lua_source': script,
+                                        'wait': 1,
+                                        'pagenum': pagenum + 1,
+                                        'url': url,
+                                    },
+                                    callback=self.parse,
+                                    cb_kwargs=kwargs)
+        except Exception as e:
+            logging.error(self.name + ": " + e.__str__())
+            logging.exception(e)
+
+    def parse_pagenum(self, response, kwargs):
+        try:
+            # 在解析页码的方法中判断是否增量爬取并设定爬取列表页数，如果运行
+            # 脚本时没有传入参数pagenum指定爬取前几页列表页，则全量爬取
+            if not self.add_pagenum:
+                return int(response.css('.pagination-roll  > ul > li:nth-last-child(3) > a::text').extract_first())
+            return self.add_pagenum
+        except Exception as e:
+            logging.error(self.name + ": " + e.__str__())
+            logging.exception(e)
+
+    def parse(self, response, **kwargs):
+        for detail in response.css('.sTradingInformationSelectedBtoList .sDisclosurLeftConDetailList'):
+            try:
+                title = detail.css('a::attr(title)').extract_first()
+                url = response.urljoin(detail.css('a::attr(href)').extract_first())
+                time = detail.css('i::text').extract_first()
+                result = {
+                    'url': url,
+                    'title': title,
+                    'time': time
+
+                }
+                yield scrapy.Request(url, callback=self.parse_item, cb_kwargs=result, dont_filter=True)
+            except Exception as e:
+                logging.error(self.name + ": " + e.__str__())
+                logging.exception(e)
+
+    def parse_item(self, response, **kwargs):
+        try:
+            title = kwargs['title']
+            if title.find('招标') >= 0:
+                category = '招标'
+            elif title.find('中标') >= 0:
+                category = '中标'
+            elif title.find('成交') >= 0:
+                category = '成交'
+            elif title.find('结果') >= 0:
+                category = '结果'
+            elif title.find('单一') >= 0:
+                category = '单一'
+            else:
+                category = '其他'
+            item = ztbkItem()
+            item['title'] = title
+            if len(response.css('#content')) > 0:
+                item['content'] = response.css('#content').extract_first()
+                item['txt'] = ''.join(response.css('#content *::text').extract())
+            elif len(response.css('#purchaseByAnno')) > 0:
+                item['content'] = response.css('#purchaseByAnno').extract_first()
+                item['txt'] = ''.join(response.css('#purchaseByAnno *::text').extract())
+            elif len(response.css('.jxTradingPublic')) > 0:
+                item['content'] = response.css('.jxTradingPublic').extract_first()
+                item['txt'] = ''.join(response.css('.jxTradingPublic *::text').extract())
+            else:
+                item['content'] = ''
+                item['txt'] = ''
+            item['appendix'] = ''
+            item['category'] = category
+            item['time'] = kwargs['time']
+            item['source'] = ''
+            item['website'] = '甘肃省公共资源交易网'
+            item['link'] = kwargs['url']
+            item['type'] = '2'
+            item['region'] = ''
+            item['appendix_name'] = ''
+            item['spider_name'] = 'gansu_ggjypt'
+            item['module_name'] = '甘肃-公共交易平台'
+            print(
+                "===========================>crawled one item" +
+                response.request.url)
+        except Exception as e:
+            logging.error(
+                self.name +
+                " in parse_item: url=" +
+                response.request.url +
+                ", exception=" +
+                e.__str__())
+            logging.exception(e)
+        yield item
+
