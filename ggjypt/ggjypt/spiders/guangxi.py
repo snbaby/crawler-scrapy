a7823acc46c78a8a97f0414da7cac71694307199
sn_baby <sn_baby@qq.com>
2019-12-08 23:20:20 +0800

    修改

--- ggjypt/ggjypt/spiders/guangxi.py
+++ ggjypt/ggjypt/spiders/guangxi.py
@@ -52,29 +52,19 @@ end
 class GansuSpider(scrapy.Spider):
     name = 'guangxi_ggjypt'
     custom_settings = {
-        'DOWNLOADER_MIDDLEWARES' : {
+        'DOWNLOADER_MIDDLEWARES': {
             'scrapy_splash.SplashCookiesMiddleware': 723,
             'scrapy_splash.SplashMiddleware': 725,
             'scrapy.downloadermiddlewares.httpcompression.HttpCompressionMiddleware': 810,
         },
-        'SPIDER_MIDDLEWARES' : {
+        'SPIDER_MIDDLEWARES': {
             'scrapy_splash.SplashDeduplicateArgsMiddleware': 100,
         },
-        'DUPEFILTER_CLASS': 'scrapy_splash.SplashAwareDupeFilter',
-        'HTTPCACHE_STORAGE' : 'scrapy_splash.SplashAwareFSCacheStorage',
-        # 'SPIDER_MIDDLEWARES': {
-        #     'scrapy_splash.SplashDeduplicateArgsMiddleware': 100,
-        # },
-        # 'DOWNLOADER_MIDDLEWARES': {
-        #     'scrapy.downloadermiddleware.useragent.UserAgentMiddleware': None,
-        #     'utils.middlewares.MyUserAgentMiddleware.MyUserAgentMiddleware': 126,
-        #     'utils.middlewares.DeduplicateMiddleware.DeduplicateMiddleware': 130,
-        # },
         'ITEM_PIPELINES': {
             'utils.pipelines.MysqlTwistedPipeline.MysqlTwistedPipeline': 64,
             'utils.pipelines.DuplicatesPipeline.DuplicatesPipeline': 100,
         },
-        # 'DUPEFILTER_CLASS': 'scrapy_splash.SplashAwareDupeFilter',
+        'DUPEFILTER_CLASS': 'scrapy_splash.SplashAwareDupeFilter',
         # 'HTTPCACHE_STORAGE': 'scrapy_splash.SplashAwareFSCacheStorage',
         'SPLASH_URL': "http://47.106.239.73:8050/"}
 
@@ -83,12 +73,61 @@ class GansuSpider(scrapy.Spider):
         self.add_pagenum = pagenum
 
     def start_requests(self):
+        test = """
+        function wait_for_element(splash, css, maxwait)
+          -- Wait until a selector matches an element
+          -- in the page. Return an error if waited more
+          -- than maxwait seconds.
+          if maxwait == nil then
+              maxwait = 10
+          end
+          return splash:wait_for_resume(string.format([[
+            function main(splash) {
+              var selector = '%s';
+              var maxwait = %s;
+              var end = Date.now() + maxwait*1000;
+
+              function check() {
+                if(document.querySelector(selector)) {
+                  splash.resume('Element found');
+                } else if(Date.now() >= end) {
+                  var err = 'Timeout waiting for element';
+                  splash.error(err + " " + selector);
+                } else {
+                  setTimeout(check, 200);
+                }
+              }
+              check();
+            }
+          ]], css, maxwait))
+        end
+        function main(splash, args)
+          splash:go(args.url)
+          splash:runjs("document.querySelector('iframe').contentWindow.document.body.querySelector('#MoreInfoList1_Pager tr td:nth-child(2) a:nth-last-child(2)').click()")
+          splash:wait(1)
+          splash:runjs("document.querySelector('body').innerHTML = document.querySelector('iframe').contentWindow.document.body.querySelector('form').innerHTML")
+          splash:wait(1)
+          return splash:html()
+        end
+        """
         try:
             url = "http://gxggzy.gxzf.gov.cn/gxzbw/showinfo/jyxx.aspx?QuYu=450001&categoryNum=001010001"
-            yield SplashRequest(url, args={'lua_source': script, 'wait': 1}, callback=self.parse_type)
+            #yield SplashRequest(url, args={'lua_source': script, 'wait': 1}, callback=self.parse_type)
+            yield SplashRequest(url,
+                                endpoint='execute',
+                                args={
+                                    'lua_source': test,
+                                    'wait': 1,
+                                    'url': url,
+                                },
+                                callback=self.test)
         except Exception as e:
             logging.error(self.name + ": " + e.__str__())
             logging.exception(e)
+
+    def test(self,response):
+        for title in response.css('#MoreInfoList1_tdcontent a::attr(title)').extract():
+            print(title)
 
     def parse_type(self, response):
         for href in response.xpath('//*[@class="LeftMenuSub"]/a/@onclick'):
