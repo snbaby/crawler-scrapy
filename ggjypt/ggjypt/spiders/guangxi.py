54dedea02af468f6b184ffb0f0d44d29702dd1f8
sn_baby <sn_baby@qq.com>
2019-12-09 00:36:03 +0800

    修改广西

--- ggjypt/ggjypt/spiders/guangxi.py
+++ ggjypt/ggjypt/spiders/guangxi.py
@@ -5,6 +5,7 @@ import logging
 from scrapy_splash import SplashRequest
 from ggjypt.items import ztbkItem
 import time
+from utils.tools.attachment import get_attachments
 
 script = """
 function wait_for_element(splash, css, maxwait)
@@ -37,14 +38,11 @@ end
 
 function main(splash, args)
   splash:go(args.url)
-  assert(splash:wait(0.1))
-  wait_for_element(splash, ".HeadStyleOfDataGridItemStyle")
-  assert(splash:wait(0.1))
-  splash:runjs("document.querySelector('.DataGrid').innerHTML = ''")
-  assert(splash:wait(0.1))
-  splash:runjs("__doPostBack('MoreInfoList1$Pager','2')")
-  assert(splash:wait(0.1))
-  wait_for_element(splash, ".HeadStyleOfDataGridItemStyle")
+  assert(splash:wait(1))
+  js = string.format("document.getElementsByTagName('iframe')[0].contentWindow.__doPostBack('MoreInfoList1$Pager','%d')", args.page)
+  splash:evaljs(js)
+  assert(splash:wait(1))
+  splash:runjs("document.querySelector('body').innerHTML = document.getElementsByTagName('iframe')[0].contentWindow.document.body.querySelector('form').innerHTML")
   return splash:html()
 end
 """
@@ -73,69 +71,20 @@ class GansuSpider(scrapy.Spider):
         self.add_pagenum = pagenum
 
     def start_requests(self):
-        test = """
-        function wait_for_element(splash, css, maxwait)
-          -- Wait until a selector matches an element
-          -- in the page. Return an error if waited more
-          -- than maxwait seconds.
-          if maxwait == nil then
-              maxwait = 10
-          end
-          return splash:wait_for_resume(string.format([[
-            function main(splash) {
-              var selector = '%s';
-              var maxwait = %s;
-              var end = Date.now() + maxwait*1000;
-
-              function check() {
-                if(document.querySelector(selector)) {
-                  splash.resume('Element found');
-                } else if(Date.now() >= end) {
-                  var err = 'Timeout waiting for element';
-                  splash.error(err + " " + selector);
-                } else {
-                  setTimeout(check, 200);
-                }
-              }
-              check();
-            }
-          ]], css, maxwait))
-        end
-        function main(splash, args)
-          splash:go(args.url)
-          splash:runjs("document.querySelector('iframe').contentWindow.document.body.querySelector('#MoreInfoList1_Pager tr td:nth-child(2) a:nth-last-child(2)').click()")
-          splash:wait(1)
-          splash:runjs("document.querySelector('body').innerHTML = document.querySelector('iframe').contentWindow.document.body.querySelector('form').innerHTML")
-          splash:wait(1)
-          return splash:html()
-        end
-        """
         try:
             url = "http://gxggzy.gxzf.gov.cn/gxzbw/showinfo/jyxx.aspx?QuYu=450001&categoryNum=001010001"
-            #yield SplashRequest(url, args={'lua_source': script, 'wait': 1}, callback=self.parse_type)
-            yield SplashRequest(url,
-                                endpoint='execute',
-                                args={
-                                    'lua_source': test,
-                                    'wait': 1,
-                                    'url': url,
-                                },
-                                callback=self.test)
+            yield SplashRequest(url, args={'lua_source': script, 'wait': 1}, callback=self.parse_type)
         except Exception as e:
             logging.error(self.name + ": " + e.__str__())
             logging.exception(e)
 
-    def test(self,response):
-        for title in response.css('#MoreInfoList1_tdcontent a::attr(title)').extract():
-            print(title)
-
     def parse_type(self, response):
+        num = 1
         for href in response.xpath('//*[@class="LeftMenuSub"]/a/@onclick'):
             for id in response.xpath('//*[@class="hubs"]/ul/li/@id'):
                 try:
                     type = href.extract().replace('Openinfo(\'','').replace('\')','')
                     url = 'http://gxggzy.gxzf.gov.cn/gxzbw/showinfo/jyxx.aspx?QuYu=' + id.extract() + '&categoryNum=' + type
-                    print(url)
                     content = {
                         'url': url
                     }
@@ -144,47 +93,45 @@ class GansuSpider(scrapy.Spider):
                         args={
                             'lua_source': script,
                             'wait': 1,
-                            'page': 1,
+                            'page': 2,
                             'url': url,
                         },
                         callback=self.parse_page,
                         cb_kwargs=content)
+
                 except Exception as e:
                     logging.error(self.name + ": " + e.__str__())
                     logging.exception(e)
 
     def parse_page(self, response, **kwargs):
-        logging.error(response.text)
-        print(response.xpath('//*[@class="DataGrid"]/tr[2]/td[2]/a/text()').extract())
-        return
-        page_count = int(response.xpath('//*[@class="totalPageNum"]').re(r'([1-9]\d*\.?\d*)')[0]) + 1
+        page_count = int(response.xpath('//*[@id="MoreInfoList1_Pager"]//b/text()').extract()[1])
         try:
             for pagenum in range(page_count):
-                if pagenum > 1:
-                    time.sleep(0.5)
-                    print('pagenum' + str(pagenum))
+                if pagenum > 0:
                     yield SplashRequest(kwargs['url'],
-                                        endpoint='execute',
-                                        args={
-                                            'lua_source': script,
-                                            'wait': 1,
-                                            'page': pagenum,
-                                            'url': kwargs['url'],
-                                        },
-                                        callback=self.parse,
-                                        cb_kwargs=kwargs)
+                        endpoint='execute',
+                        args={
+                            'lua_source': script,
+                            'wait': 1,
+                            'page': pagenum,
+                            'url': kwargs['url'],
+                        },
+                        callback=self.parse,
+                        cb_kwargs=kwargs)
         except Exception as e:
             logging.error(self.name + ": " + e.__str__())
             logging.exception(e)
 
     def parse(self, response, **kwargs):
-        for selector in response.xpath('//*[@id="content"]/tr'):
+        for selector in response.xpath('//*[@class="DataGrid"]/tbody/tr'):
             try:
-                item = {}
-                item['title'] = selector.xpath('./td[2]/a/text()').extract_first()
-                item['time'] = selector.xpath('./td[3]/text()').extract_first()
-                url = response.urljoin(selector.xpath('./td[2]/a/@href').extract_first())
-                yield scrapy.Request(url,callback=self.parse_item, dont_filter=True, cb_kwargs=item)
+                if selector.xpath('./td[2]/a/text()'):
+                    item = {}
+                    item['title'] = selector.xpath('./td[2]/a/text()').extract_first()
+                    item['time'] = selector.xpath('./td[3]/text()').extract_first()
+                    url = 'http://gxggzy.gxzf.gov.cn' + selector.xpath('./td[2]/a/@href').extract_first()
+                    item['url'] = url
+                    yield scrapy.Request(url,callback=self.parse_item, dont_filter=True, cb_kwargs=item)
             except Exception as e:
                 logging.error(self.name + ": " + e.__str__())
                 logging.exception(e)
@@ -192,6 +139,7 @@ class GansuSpider(scrapy.Spider):
     def parse_item(self, response, **kwargs):
         if kwargs['title']:
             try:
+                appendix, appendix_name = get_attachments(response)
                 category = '其他';
                 title = kwargs['title']
                 if title.find('招标') >= 0:
@@ -206,8 +154,8 @@ class GansuSpider(scrapy.Spider):
                     category = '单一'
                 item = ztbkItem()
                 item['title'] = title
-                item['content'] = "".join(response.xpath('//div[@class="message clearFloat jyxx"]').extract())
-                item['source'] = response.xpath('//a[@class="originUrl"]/text()').extract_first()
+                item['content'] = "".join(response.xpath('//div[@class="infodetail"]').extract())
+                item['source'] = ''
                 item['category'] = category
                 item['type'] = ''
                 item['region'] = '山西省'
@@ -215,10 +163,10 @@ class GansuSpider(scrapy.Spider):
                 item['website'] = '山西省公共资源交易服务平台'
                 item['module_name'] = '山西省-公共交易平台'
                 item['spider_name'] = 'shanxi_ggjypt'
-                item['txt'] = "".join(response.xpath('//div[@class="message clearFloat jyxx"]//text()').extract())
-                item['appendix_name'] = ";".join(response.xpath('//div[@class="message clearFloat jyxx"]//a[contains(@href,"pdf") or contains(@href,"doc") or contains(@href,"docx") or contains(@href,"xls")]/text()').extract())
-                item['link'] = response.request.url
-                item['appendix'] = ";".join(response.xpath('//div[@class="message clearFloat jyxx"]//a[contains(@href,"pdf") or contains(@href,"doc") or contains(@href,"docx") or contains(@href,"xls")]/@href').extract())
+                item['txt'] = "".join(response.xpath('//div[@class="infodetail"]//text()').extract())
+                item['appendix_name'] = appendix_name
+                item['link'] = kwargs['url']
+                item['appendix'] = appendix
                 print(
                     "===========================>crawled one item" +
                     response.request.url)
