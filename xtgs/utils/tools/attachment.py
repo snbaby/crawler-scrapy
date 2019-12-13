bb99ac67be1b719912d24d05b03e6f6e2380f098
sn_baby <sn_baby@qq.com>
2019-12-13 22:37:00 +0800

    添加时间格式化方法

--- xtgs/utils/tools/attachment.py
+++ xtgs/utils/tools/attachment.py
@@ -1,6 +1,6 @@
 import scrapy
 import logging
-
+import re
 
 def get_attachments(response):
     valid_extensions = [".doc", ".docx", ".xlsx", ".xls", ".pdf", ".zip", "wps", ".rar"]
@@ -22,3 +22,15 @@ def get_attachments(response):
                 appendix_name=appendix_name + text +";"
     return appendix, appendix_name
 
+#时间格式化
+def get_times(srcTime):
+    list = re.findall(r'([1-9]\d*?\d*)', srcTime)
+    result = ''
+    if len(list) == 1 and len(list[0]) == 8:  # eg:20190810
+        result = list[0][:4] + '-' + list[0][4:6] + '-' + list[0][6:]
+    elif len(list) > 2:
+        result = list[0] + '-' + list[1].zfill(2) + '-' + list[2].zfill(2)
+    else:
+        logging.error('时间格式化异常：' + srcTime)
+        result = srcTime
+    return result
