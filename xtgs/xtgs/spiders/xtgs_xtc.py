93b9b5b2700b0ee901d221938d361b902ea3b04d
sn_baby <sn_baby@qq.com>
2019-12-15 13:55:32 +0800

    添加信托池

--- xtgs/xtgs/spiders/xtgs_xtc.py
+++ xtgs/xtgs/spiders/xtgs_xtc.py
@@ -18,7 +18,7 @@ end
 
 
 class TianJinSzfwjSpider(scrapy.Spider):
-    name = 'xtgs_jfz'
+    name = 'xtgs_xtc'
     custom_settings = {
         'SPIDER_MIDDLEWARES': {
             'scrapy_splash.SplashDeduplicateArgsMiddleware': 100,
@@ -42,7 +42,7 @@ class TianJinSzfwjSpider(scrapy.Spider):
 
     def start_requests(self):
         try:
-            url = "https://www.jfz.com/xintuo/company-p1.html"
+            url = "https://www.xintuochi.com/xtgs/?_p1.html"
             yield SplashRequest(url, args={'lua_source': script, 'wait': 1}, callback=self.parse_page)
         except Exception as e:
             logging.error(self.name + ": " + e.__str__())
@@ -54,7 +54,7 @@ class TianJinSzfwjSpider(scrapy.Spider):
         try:
             for pagenum in range(page_count):
                 if pagenum > 0:
-                    url = 'https://www.jfz.com/xintuo/company-p' + str(pagenum) + '.html'
+                    url = 'https://www.xintuochi.com/xtgs/?_p' + str(pagenum) + '.html'
                     yield SplashRequest(url, args={'lua_source': script, 'wait': 1}, callback=self.parse, dont_filter=True)
         except Exception as e:
             logging.error(self.name + ": " + e.__str__())
@@ -65,20 +65,26 @@ class TianJinSzfwjSpider(scrapy.Spider):
             # 在解析页码的方法中判断是否增量爬取并设定爬取列表页数，如果运行
             # 脚本时没有传入参数pagenum指定爬取前几页列表页，则全量爬取
             if not self.add_pagenum:
-                return int(response.xpath('//*[@class="pagination"]/li[last()]/a/@href').re(r'([1-9]\d*?\d*)')[0]) + 1
+                return int(response.xpath('//*[@class="assxx_fy"]/ul/li[last()-1]/a/@href').re(r'([1-9]\d*?\d*)')[0]) + 1
             return self.add_pagenum
         except Exception as e:
             logging.error(self.name + ": " + e.__str__())
             logging.exception(e)
 
     def parse(self, response):
-        for selector in response.xpath('//table[@class="xt_pro_tb_2"]/tbody/tr'):
+        for selector in response.xpath('//form[@class="ff"]/table/tr[position()>1]'):
             try:
                 item = {}
-                url = response.urljoin(selector.xpath('./td[1]/div/a/@href').extract_first())
+                url = response.urljoin(selector.xpath('./td[1]/a/@href').extract_first())
                 print(url)
-                item['avg_yield'] = selector.xpath('./td[2]/span/text()').extract_first()
+                item['avg_yield'] = selector.xpath('./td[2]/text()').extract_first()
                 item['pro_hold_rate'] = selector.xpath('./td[3]/text()').extract_first()
+                item['simple_name'] = selector.xpath('./td[1]/a/text()').extract_first()
+                item['registe_money'] = selector.xpath('./td[4]/text()').extract_first()
+                item['partner_bg'] = selector.xpath('./td[5]/text()').extract_first()
+                item['legal_person'] = selector.xpath('./td[6]/text()').extract_first()
+                item['address'] = selector.xpath('./td[7]/text()').extract_first()
+                item['create_date'] = selector.xpath('./td[8]/text()').extract_first()
                 yield SplashRequest(url,callback=self.parse_item, dont_filter=True,cb_kwargs=item)
             except Exception as e:
                 logging.error(self.name + ": " + e.__str__())
@@ -91,24 +97,21 @@ class TianJinSzfwjSpider(scrapy.Spider):
         print(kwargs)
         try:
             item = xtgsItem()
-            name = response.xpath('//table[@class="xt_detail_tb_1"]/tbody/tr[1]/td[2]/text()').extract_first()
-            simple_name = response.xpath('//table[@class="xt_detail_tb_1"]/tbody/tr[1]/td[4]/text()').extract_first()
-            en_name = response.xpath('//table[@class="xt_detail_tb_1"]/tbody/tr[2]/td[2]/text()').extract_first()
-            create_date = response.xpath('//table[@class="xt_detail_tb_1"]/tbody/tr[3]/td[2]/text()').extract_first()
-            registe_money = response.xpath('//table[@class="xt_detail_tb_1"]/tbody/tr[3]/td[4]/text()').extract_first()
-            address = response.xpath('//table[@class="xt_detail_tb_1"]/tbody/tr[4]/td[2]/text()').extract_first()
-            legal_person = response.xpath('//table[@class="xt_detail_tb_1"]/tbody/tr[5]/td[4]/text()').extract_first()
-            dongshizhang = response.xpath('//table[@class="xt_detail_tb_1"]/tbody/tr[4]/td[4]/text()').extract_first()
-            shareholder = response.xpath('//table[@class="xt_detail_tb_1"]/tbody/tr[6]/td[2]/text()').extract_first()
-            general_manager = response.xpath('//table[@class="xt_detail_tb_1"]/tbody/tr[5]/td[2]/text()').extract_first()
-            is_ipo = response.xpath('//table[@class="xt_detail_tb_1"]/tbody/tr[6]/td[4]/text()').extract_first()
-            company_intro = response.xpath('//div[@class="xt_com_detail_box"]/div[2]/p/text()').extract_first()
-            partner_bg = response.xpath('//ul[@class="attr_list clearfix"]/li[1]/div[1]/text()').extract_first()
-            aum = ''.join(response.xpath('//ul[@class="attr_list clearfix"]/li[3]/div[1]//text()').extract())
+            name = response.xpath('//div[@class="gsxq-table"]/table/tr[2]/td[2]/span/text()').extract_first()
+            is_ipo = response.xpath('//div[@class="gsxq-table"]/table/tr[6]/td[4]/span/text()').extract_first()
+            dongshizhang = response.xpath('//div[@class="gsxq-table"]/table/tr[4]/td[4]/span/text()').extract_first()
+            shareholder = response.xpath('//div[@class="gsxq-table"]/table/tr[6]/td[2]/span/text()').extract_first()
+            general_manager = response.xpath('//div[@class="gsxq-table"]/table/tr[5]/td[2]/span/text()').extract_first()
+            simple_name = kwargs['simple_name']
+            create_date = kwargs['create_date']
+            registe_money = kwargs['registe_money']
+            address = kwargs['address']
+            legal_person = kwargs['legal_person']
+            partner_bg = kwargs['partner_bg']
             print(create_date)
             item['name'] = name  # 公司名称
             item['simple_name'] = simple_name  # 公司简称
-            item['en_name'] = en_name  # 英文名称
+            item['en_name'] = ''  # 英文名称
             item['create_date'] = get_times(create_date)  # 成立日期
             item['address'] = address  # 所在地
             item['registe_money'] = registe_money  # 注册资本
@@ -117,22 +120,22 @@ class TianJinSzfwjSpider(scrapy.Spider):
             item['regist_address'] = ''  # 注册地址
             item['partner_compose'] = ''  # 股东构成
             item['partner_bg'] = partner_bg  # 股东背景
-            item['company_intro'] = company_intro  # 公司简介
+            item['company_intro'] = ''.join(response.xpath('//p[@class="PD20"]//text()').extract())  # 公司简介
             item['legal_person'] = legal_person  # 法人代表
             item['dongshizhang'] = dongshizhang  # 董事长
             item['shareholder'] = shareholder  # 大股东
             item['general_manager'] = general_manager  # 总经理
-            item['aum'] = aum  # 资产管理规模
+            item['aum'] = response.xpath('//div[@class="gsxq_top"]/ul/li[4]/h2/text()').extract_first()  # 资产管理规模
             item['avg_yield'] = kwargs['avg_yield']  # 平均收益率
             item['pro_hold_rate'] = kwargs['pro_hold_rate']   # 产品兑付比例
             item['company_website'] = ''  # 公司网址
             item['telephone'] = ''  # 电话
             item['fax'] = ''  # 传真
             item['postcode'] = ''  # 邮编
-            item['website'] = '金斧子'  # 数据来源网站
+            item['website'] = '信托池'  # 数据来源网站
             item['link'] = response.request.url  # 数据源链接
-            item['spider_name'] = 'xtgs_jfz'  # 名称
-            item['module_name'] = '信托公司_金斧子'  # 模块名称
+            item['spider_name'] = 'xtgs_xtc'  # 名称
+            item['module_name'] = '信托公司_信托池'  # 模块名称
 
             print(
                 "===========================>crawled one item" +
