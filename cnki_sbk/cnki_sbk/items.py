66dea84b7635a6e9ecfbf8c12c8c3ec315096934
Li Yi <lovelylich@gmail.com>
2019-12-22 23:57:06 +0800

    add cnki-sbk

---
+++ cnki_sbk/cnki_sbk/items.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://doc.scrapy.org/en/latest/topics/items.html
+import re
+from urllib import parse
+from scrapy.item import Field
+import scrapy
+from scrapy.loader.processors import TakeFirst, MapCompose, Join
+
+
+class cnki_sbkItem(scrapy.Item):
+    title = scrapy.Field()
+    financers = scrapy.Field()
+    investors = scrapy.Field()
+    amount = scrapy.Field()
+    rotation = scrapy.Field()
+    time = scrapy.Field()
+    industry = scrapy.Field()
+    introduction = scrapy.Field()
+    website = scrapy.Field()
+    link = scrapy.Field()
+    content = scrapy.Field()
+    spider_name = scrapy.Field()
+    module_name = scrapy.Field()
---
+++ cnki_sbk/cnki_sbk/spiders/sbk.py
@@ -0,0 +1,115 @@
+# -*- coding: utf-8 -*-
+import scrapy
+import logging
+import json
+import time
+
+from cnki_sbk.items import  cnki_sbkItems
+
+
+class SbkSpider(scrapy.Spider):
+    name = 'sbk'
+    custom_settings = {
+        'DOWNLOADER_MIDDLEWARES': {
+            'scrapy_splash.SplashCookiesMiddleware': 723,
+            'scrapy_splash.SplashMiddleware': 725,
+            'scrapy.downloadermiddlewares.httpcompression.HttpCompressionMiddleware': 810,
+        },
+        'SPIDER_MIDDLEWARES': {
+            'scrapy_splash.SplashDeduplicateArgsMiddleware': 100,
+        },
+        'ITEM_PIPELINES': {
+            'utils.pipelines.MysqlTwistedPipeline.MysqlTwistedPipeline': 64,
+            'utils.pipelines.DuplicatesPipeline.DuplicatesPipeline': 100,
+        },
+        'DUPEFILTER_CLASS': 'scrapy_splash.SplashAwareDupeFilter',
+        # 'HTTPCACHE_STORAGE': 'scrapy_splash.SplashAwareFSCacheStorage',
+        'SPLASH_URL': "http://47.106.239.73:8050/"}
+
+    def __init__(self, pagenum=None, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.add_pagenum = pagenum
+
+    def start_requests(self):
+        try:
+            requrl = 'http://kns.cnki.net/kns/brief/brief.aspx?pagename=ASP.brief_result_aspx&isinEn=0&dbPrefix=CDMD&dbCatalog=%e4%b8%ad%e5%9b%bd%e4%bc%98%e7%a7%80%e5%8d%9a%e7%a1%95%e5%a3%ab%e5%ad%a6%e4%bd%8d%e8%ae%ba%e6%96%87%e5%85%a8%e6%96%87%e6%95%b0%e6%8d%ae%e5%ba%93&ConfigFile=CDMD.xml&research=off&t=1577027019127&keyValue=&S=1&sorttype='
+            yield scrapy.Request(requrl,  callback=self.parse_page, dont_filter=True)
+        except Exception as e:
+            logging.error(self.name + ": " + e.__str__())
+            logging.exception(e)
+
+    def parse_page(self, response, **kwargs):
+        page_count = self.parse_pagenum(response, kwargs)
+        try:
+            for content in contents:
+                for pagenum in range(page_count):
+                    data = {
+                        "total": 0,
+                        "per_page": 20,
+                        "page": pagenum + 1,
+                        "type": 1,
+                        "scope": "",
+                        "sub_scope": "",
+                        "round": [],
+                        "valuation": [],
+                        "valuations": "",
+                        "ipo_platform": "",
+                        "equity_ratio": "",
+                        "status": "",
+                        "prov": "",
+                        "city": [],
+                        "time": [],
+                        "selected": "",
+                        "location": "",
+                        "hot_city": "",
+                        "currency": [],
+                        "keyword": ""}
+                    yield scrapy.FormRequest(content['url'], cb_kwargs=content, body=json.dumps(data), method='POST', headers=header, callback=self.parse, dont_filter=True)
+        except Exception as e:
+            logging.error(self.name + ": " + e.__str__())
+            logging.exception(e)
+
+    def parse_pagenum(self, response, kwargs):
+        try:
+            # 在解析页码的方法中判断是否增量爬取并设定爬取列表页数，如果运行
+            # 脚本时没有传入参数pagenum指定爬取前几页列表页，则全量爬取
+            if not self.add_pagenum:
+                return 1
+            return self.add_pagenum
+        except Exception as e:
+            logging.error(self.name + ": " + e.__str__())
+            logging.exception(e)
+
+    def parse(self, response, **kwargs):
+        investevents = json.loads(response.text)['data']['data']
+        for investevent in investevents:
+            try:
+                item = cnki_sbkItems()
+                item['title'] = investevent['invse_title']
+                item['financers'] = investevent['name']
+                investors = ''
+                for investor in investevent['investor']:
+                    investors = investors + investor['name'] + ','
+                item['investors'] = investors
+                item['amount'] = investevent['money']
+                item['rotation'] = investevent['round']
+                item['time'] = str(investevent['year'])+'-'+str(investevent['month'])+'-'+str(investevent['day'])
+                item['industry'] = investevent['com_scope']
+                item['introduction'] = investevent['com_des']
+                item['website'] = 'IT桔子'
+                item['link'] = 'https://www.itjuzi.com/investevent/'+str(investevent['id'])
+                item['content'] = investevent['invse_des']
+                item['spider_name'] = 'rz'
+                item['module_name'] = 'IT桔子-融资'
+                print(
+                    "===========================>crawled one item" +
+                    response.request.url)
+            except Exception as e:
+                logging.error(
+                    self.name +
+                    " in parse_item: url=" +
+                    response.request.url +
+                    ", exception=" +
+                    e.__str__())
+                logging.exception(e)
+            yield item
