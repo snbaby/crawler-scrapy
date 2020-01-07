1c9a0c669d9e16be20396171fa1bcd1f6390bbea
sn_baby <sn_baby@qq.com>
2020-01-08 01:03:10 +0800

    修改配置

--- zfcgw/zfcgw/settings.py
+++ zfcgw/zfcgw/settings.py
@@ -98,18 +98,19 @@ LOG_ENABLED = True
 LOG_FILE = "spiders.log"
 LOG_LEVEL = "ERROR"
 
-# MYSQL_HOST = '10.8.32.125'
-MYSQL_HOST = 'rm-8vbif49m6k7l651e5fo.mysql.zhangbei.rds.aliyuncs.com'
+
+MYSQL_HOST = '10.8.32.125'
+# MYSQL_HOST = 'rm-8vbif49m6k7l651e5fo.mysql.zhangbei.rds.aliyuncs.com'
 # MYSQL_HOST = '192.168.43.201'
 MYSQL_USER = 'root'
-MYSQL_PASSWORD = 'Liqin1988'
-# MYSQL_PASSWORD ='yeyang112114'
+#MYSQL_PASSWORD = 'Liqin1988'
+MYSQL_PASSWORD ='Admin123!'
 
-MYSQL_DB = 'python'
+MYSQL_DB = 'engineering-brain'
 MYSQL_PORT = 3306
 MYSQL_CHRSET = 'utf8'
 
-SPLASH_URL = "http://39.100.240.19:8050/"
+SPLASH_URL = "http://localhost:8050/"
 
 # REDIS_HOST = '10.8.32.123'
 # REDIS_HOST = '127.0.0.1'
