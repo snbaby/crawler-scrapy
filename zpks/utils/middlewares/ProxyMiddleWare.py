42d72cbfc934b5208cea98f2570a1767aece774c
sn_baby <sn_baby@qq.com>
2020-01-08 20:41:42 +0800

    修改配置

--- zpks/utils/middlewares/ProxyMiddleWare.py
+++ zpks/utils/middlewares/ProxyMiddleWare.py
@@ -1,29 +1,31 @@
 import requests
+import logging
 
 class ProxyMiddleWare(object):
     """docstring for ProxyMiddleWare"""
 
     def process_request(self, request, spider):
-        proxy = self.get_random_proxy()
         '''对request对象加上proxy'''
-        print("this is request ip:" +proxy)
+        proxy = self.get_random_proxy()
+        logging.info("this is request ip:" + proxy)
         try:
-            request.meta['proxy'] = 'http://'+ proxy
+            request.meta['splash']['args']['proxy'] = 'https://' + proxy
+            logging.info('this is splashrequest!!!')
         except:
-            request.meta['splash']['args']['proxy'] ='http://'+ proxy
-
+            request.meta['proxy'] = 'http://' + proxy
 
     def process_response(self, request, response, spider):
+        '''对返回的response处理'''
         # 如果返回的response状态不是200，重新生成当前request对象
         if response.status != 200:
             proxy = self.get_random_proxy()
-            print("this is respo"
-                  "nse ip:" + proxy)
+            logging.info("this is response ip:" + proxy)
             # 对当前reque加上代理
-            request.meta['proxy'] ='http://'+ proxy
+            request.meta['proxy'] = proxy
             return request
         return response
 
     def get_random_proxy(self):
-        '''随机从IP数据库中读取proxy'''
-        return requests.get('http://10.8.32.124:5010/get').content.decode('utf-8')
+        # logging.info('proxy IP:' + requests.get('http://10.8.32.123:5010/get').text)
+        # return  requests.get('http://10.8.32.123:5010/get').text
+        return  requests.get('http://127.0.0.1:5010/get/').text
