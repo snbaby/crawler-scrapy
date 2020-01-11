5c628a0d3a1673e3f143b7e02fa18fea4a6dbc9d
sn_baby <sn_baby@qq.com>
2020-01-11 10:26:40 +0800

    添加科技报告

---
+++ cnki_kjbg/cnki_kjbg/spiders/test1.py
@@ -0,0 +1,21 @@
+# from datetime import datetime
+# from threading import Timer
+# pages = 10
+# starts = 0
+# # 打印时间函数
+# def printTime(inc,text):
+#     for i in range(text,text+10):
+#         text+=1
+#         print(i)
+#     t = Timer(inc, printTime,(inc,text))
+#     t.start()
+#
+#
+# # 打印时间函数
+# # def printTime(inc,text):
+# #     print(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+# #     print(text)
+# #     t = Timer(inc, printTime,(inc,text,))
+# #     t.start()
+# #
+# printTime(2, starts)
