42b2eea3422947969275fe0703ca3b3d60536cd7
sn_baby <sn_baby@qq.com>
2020-01-15 23:14:30 +0800

    修改

--- bzk_dfbz/bzk_dfbz/main.py
+++ bzk_dfbz/bzk_dfbz/main.py
@@ -1,1 +1,1 @@
-# -*- coding: utf-8 -*-import sys,osfrom scrapy.cmdline import executesys.path.append(os.path.abspath(os.path.dirname(__file__)))os.system("scrapy crawl dfbz -a pagenum=10")
+# -*- coding: utf-8 -*-import sys,osfrom scrapy.cmdline import executesys.path.append(os.path.abspath(os.path.dirname(__file__)))os.system("scrapy crawl dfbz")
