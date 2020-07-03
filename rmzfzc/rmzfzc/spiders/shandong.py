00f06f6d992b5ce3d3aa92e0d37157f732c8acfa
sn_baby <sn_baby@qq.com>
2020-07-03 11:55:58 +0800

    154	rmzfzc	shandong	山东省人民政府-政策文件	政策新闻	5			部分时间未爬取到		http://www.shandong.gov.cn/col/col2262/index.html

--- rmzfzc/rmzfzc/spiders/shandong.py
+++ rmzfzc/rmzfzc/spiders/shandong.py
@@ -18,7 +18,7 @@ function main(splash, args)
 end
 """
 
-url = 'http://www.shandong.gov.cn/module/web/jpage/dataproxy.jsp?startrecord=1&endrecord=6000&perpage=6000'
+url = 'http://www.shandong.gov.cn/module/web/jpage/dataproxy.jsp'
 
 class AnhuiSpider(scrapy.Spider):
     name = 'shandong'
@@ -55,35 +55,48 @@ class AnhuiSpider(scrapy.Spider):
             contents = [
                 {
                     'topic':'zcjd',
-                    'data':{'col': '1','webid': '4','path': 'http://www.shandong.gov.cn/','columnid': '2262','sourceContentType': '1','unitid': '5836','webname': '山东省人民政府','permissiontype': '0'}
-                },
+                    'data':{'col': '1','webid': '410','path': 'http://www.shandong.gov.cn/','columnid': '107868','sourceContentType': '1','unitid': '240818','webname': '山东省人民政府','permissiontype': '0'}
+                }
+                ,
                 {
                     'topic': 'zcwj',
-                    'data': {'col': '1','webid': '4','path': 'http://www.shandong.gov.cn/','columnid': '2266','sourceContentType': '1','unitid': '5836','webname': '山东省人民政府','permissiontype': '0'}
-                },
-                {
-                    'topic': 'zcwj',
-                    'data': {'col': '1','webid': '4','path': 'http://www.shandong.gov.cn/','columnid': '2267','sourceContentType': '1','unitid': '5836','webname': '山东省人民政府','permissiontype': '0'}
-                },
-                {
-                    'topic': 'zcwj',
-                    'data': {'col': '1','webid': '4','path': 'http://www.shandong.gov.cn/','columnid': '2268','sourceContentType': '1','unitid': '5836','webname': '山东省人民政府','permissiontype': '0'}
+                    'data': {'col': '1','webid': '410','path': 'http://www.shandong.gov.cn/','columnid': '107851','sourceContentType': '1','unitid': '283644','webname': '山东省人民政府','permissiontype': '0'}
                 }
             ]
             for content in contents:
-                yield scrapy.FormRequest(url, formdata=content['data'],
-                                         headers={'Content-Type': 'application/x-www-form-urlencoded'},
-                                         callback=self.parse_page,meta=content)
+                path = url + '?startrecord=1&endrecord=45&perpage=15'
+                yield scrapy.FormRequest(path, formdata=content['data'],
+                                         headers={
+                                                    'Content-Type': 'application/x-www-form-urlencoded'
+                                                  },
+                                         callback=self.parse_page,meta=content,dont_filter=True)
         except Exception as e:
             logging.error(self.name + ": " + e.__str__())
             logging.exception(e)
 
     def parse_page(self, response):
-        text = response.text.replace('<![CDATA[', '').replace(']]>','').replace(
-            '<datastore>', '').replace('<nextgroup>', '').replace('<recordset>', '').replace('</recordset>','').replace('</datastore>','').replace(
-            '<record>', '').replace('</nextgroup>', '')
-        sel = scrapy.Selector(text=text)
-        for href in sel.xpath('//*[@class="tit"]/a/@href').extract():
+        try:
+            total = int(self.parse_total(response))
+            i = 0
+            while i*45 < total:
+                i = i + 1
+                endrecord = i*45
+                if endrecord > total:
+                    endrecord = total
+                startrecord = (i-1)*45 + 1
+                path = url + '?startrecord='+str(startrecord)+'&endrecord='+str(endrecord)+'&perpage=15'
+                yield scrapy.FormRequest(path, formdata=response.meta['data'],
+                                         headers={
+                                             'Content-Type': 'application/x-www-form-urlencoded'
+                                         },
+                                         callback=self.parse, meta=response.meta,dont_filter=True)
+        except Exception as e:
+            logging.error(self.name + ": " + e.__str__())
+            logging.exception(e)
+
+    def parse(self, response):
+        sel = scrapy.Selector(text=response.text)
+        for href in sel.css('a::attr(href)').extract():
             try:
                 yield scrapy.Request(href, callback=self.parse_item,
                                      dont_filter=True, meta=response.meta)
@@ -91,65 +104,69 @@ class AnhuiSpider(scrapy.Spider):
                 logging.error(self.name + ": " + e.__str__())
                 logging.exception(e)
 
+    def parse_total(self, response):
+        sel = scrapy.Selector(text=response.text)
+        try:
+            # 在解析页码的方法中判断是否增量爬取并设定爬取列表页数，如果运行
+            # 脚本时没有传入参数pagenum指定爬取前几页列表页，则全量爬取
+            if not self.add_pagenum:
+                return int(sel.css('totalrecord::text').extract_first())
+            return self.add_pagenum
+        except Exception as e:
+            logging.error(self.name + ": " + e.__str__())
+            logging.exception(e)
+
     def parse_item(self, response):
         try:
             item = rmzfzcItem()
             appendix, appendix_name = get_attachments(response)
             if response.meta['topic'] == 'zcjd':
-                item['title'] = response.xpath(
-                    '//*[@class="xq-tit"]/text()').extract_first()
+                item['title'] = response.css('meta[name=ArticleTitle]::attr(content)').extract_first()
                 item['article_num'] = ''
-                source = response.xpath('//*[@class="pl42"]/text()').extract_first()
-                item['source'] = source.replace('来源：', '') if source else ''
-                timeStr = response.xpath('//*[@class="R-tit"]/text()').extract_first() if response.xpath(
-                    '//*[@class="R-tit"]/text()') else response.xpath('//*[@class="R-tit"]/span/text()').extract_first()
-                item['time'] = timeStr.replace('时间：', '')
-                item['content'] = "".join(response.xpath('//div[@class="article"]').extract())
+                item['source'] = response.css('meta[name=contentSource]::attr(content)').extract_first()
+                if response.css('meta[name=pubdate]::attr(content)'):
+                    item['time'] = get_times(response.css('meta[name=pubdate]::attr(content)').extract_first())
+                else:
+                    item['time'] = get_times(response.css('meta[name=PubDate]::attr(content)').extract_first())
+                item['content'] = "".join(response.css('.wip_art_con').extract())
                 item['appendix'] = appendix
                 item['province'] = '山东省'
                 item['city'] = ''
                 item['area'] = ''
                 item['website'] = '山东省人民政府'
                 item['link'] = response.request.url
-                item['txt'] = "".join(response.xpath('//div[@class="article"]//text()').extract())
+                item['txt'] = "".join(response.css('.wip_art_con *::text').extract())
                 item['appendix_name'] = appendix_name
                 item['module_name'] = '山东省人民政府'
                 item['spider_name'] = 'shandong_zcjd'
-                item['time'] = get_times(item['time'])
             else:
+                item['title'] = response.css('meta[name=ArticleTitle]::attr(content)').extract_first()
+                if response.css('meta[name=pubdate]::attr(content)'):
+                    item['time'] = get_times(response.css('meta[name=pubdate]::attr(content)').extract_first())
+                else:
+                    item['time'] = get_times(response.css('meta[name=PubDate]::attr(content)').extract_first())
                 if response.xpath('//div[@class="people-desc"]'):
                     article_num = response.xpath(
                         '//*[@class="people-desc"]/table/tbody/tr[3]/td[1]/text()').extract_first()
-                    item['title'] = response.xpath(
-                        '//*[@class="people-desc"]/table/tbody/tr[2]/td[1]/text()').extract_first()
                     item['article_num'] = article_num.strip() if article_num else ''
-                    item['source'] = response.xpath(
-                        '//*[@class="people-desc"]/table/tbody/tr[1]/td[1]/text()').extract_first()
-                    item['time'] = response.xpath(
-                        '//*[@class="people-desc"]/table/tbody/tr[3]/td[2]/text()').extract_first()
                 else:
-                    item['title'] = response.xpath(
-                        '//*[@class="xq-tit"]/text()').extract_first()
                     item['article_num'] = ''
-                    source = response.xpath('//*[@class="pl42"]/text()').extract_first()
-                    item['source'] = source.replace('来源：','') if source else ''
-                    timeStr = response.xpath('//*[@class="R-tit"]/text()').extract_first() if response.xpath('//*[@class="R-tit"]/text()') else response.xpath('//*[@class="R-tit"]/span/text()').extract_first()
-                    item['time'] = timeStr.replace('时间：','')
                 item['appendix'] = appendix
-                item['content'] = "".join(response.xpath('//div[@class="article"]').extract())
+                item['content'] = "".join(response.xpath('//div[@class="wip_art_conbg"]').extract())
+                item['source'] = response.css('meta[name=contentSource]::attr(content)').extract_first()
                 item['province'] = '山东省'
                 item['city'] = ''
                 item['area'] = ''
                 item['website'] = '山东省人民政府'
                 item['link'] = response.request.url
-                item['txt'] = "".join(response.xpath('//div[@class="article"]//text()').extract())
+                item['txt'] = "".join(response.xpath('//div[@class="wip_art_conbg"]//text()').extract())
                 item['appendix_name'] = appendix_name
                 item['module_name'] = '山东省人民政府'
                 item['spider_name'] = 'shandong_zfwj'
                 item['time'] = get_times(item['time'])
-            print(
-                "===========================>crawled one item" +
-                response.request.url)
+
+            if not response.css('meta[name=ArticleTitle]::attr(content)'):
+                return
         except Exception as e:
             logging.error(
                 self.name +
