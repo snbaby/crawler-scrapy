bc0804e8b76986ad6f1e70503aa322a73844d88a
sn_baby <sn_baby@qq.com>
2020-09-11 11:33:30 +0800

    修改

--- ggjypt/ggjypt/spiders/guangxi.py
+++ ggjypt/ggjypt/spiders/guangxi.py
@@ -5,7 +5,7 @@ import logging
 from scrapy_splash import SplashRequest
 from ggjypt.items import ztbkItem
 import time
-from utils.tools.attachment import get_attachments,get_times
+from utils.tools.attachment import get_attachments, get_times
 
 script = """
 function wait_for_element(splash, css, maxwait)
@@ -39,13 +39,17 @@ end
 function main(splash, args)
   splash:go(args.url)
   assert(splash:wait(1))
-  js = string.format("document.getElementsByTagName('iframe')[0].contentWindow.__doPostBack('MoreInfoList1$Pager','%d')", args.page)
+  splash:runjs("document.querySelector('#MoreInfoList1_tdcontent').innerHTML=''")
+  js = string.format("__doPostBack('MoreInfoList1$Pager','%d')", args.page)
   splash:evaljs(js)
-  assert(splash:wait(1))
-  splash:runjs("document.querySelector('body').innerHTML = document.getElementsByTagName('iframe')[0].contentWindow.document.body.querySelector('form').innerHTML")
+  wait_for_element(splash, ".DataGrid")
+  assert(splash:wait(5))
   return splash:html()
 end
 """
+
+url = "http://gxggzy.gxzf.gov.cn/gxzbw/showinfo/MoreInfo.aspx?QuYu=450001&categoryNum=001004001"
+
 
 class GansuSpider(scrapy.Spider):
     name = 'guangxi_ggjypt'
@@ -68,7 +72,7 @@ class GansuSpider(scrapy.Spider):
         },
         'DUPEFILTER_CLASS': 'scrapy_splash.SplashAwareDupeFilter',
         # 'HTTPCACHE_STORAGE': 'scrapy_splash.SplashAwareFSCacheStorage',
-        'SPLASH_URL': "http://localhost:8050/"}
+        'SPLASH_URL': "http://47.57.108.128:8050/"}
 
     def __init__(self, pagenum=None, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -76,75 +80,51 @@ class GansuSpider(scrapy.Spider):
 
     def start_requests(self):
         try:
-            url = "http://gxggzy.gxzf.gov.cn/gxzbw/showinfo/jyxx.aspx?QuYu=450001&categoryNum=001010001"
-            yield SplashRequest(url, args={'lua_source': script, 'wait': 1}, callback=self.parse_type)
+            yield SplashRequest(url, args={'lua_source': script, 'wait': 1, 'page': 1, 'url': url, },
+                                callback=self.parse_page, dont_filter=True)
         except Exception as e:
             logging.error(self.name + ": " + e.__str__())
             logging.exception(e)
-
-    def parse_type(self, response):
-        for href in response.xpath('//*[@class="LeftMenuSub"]/a/@onclick'):
-            for id in response.xpath('//*[@class="hubs"]/ul/li/@id'):
-                try:
-                    type = href.extract().replace('Openinfo(\'','').replace('\')','')
-                    url = 'http://gxggzy.gxzf.gov.cn/gxzbw/showinfo/jyxx.aspx?QuYu=' + id.extract() + '&categoryNum=' + type
-                    content = {
-                        'url': url
-                    }
-                    yield SplashRequest(url,
-                        endpoint='execute',
-                        args={
-                            'lua_source': script,
-                            'wait': 1,
-                            'page': 2,
-                            'url': url,
-                        },
-                        callback=self.parse_page,
-                        meta=content)
-
-                except Exception as e:
-                    logging.error(self.name + ": " + e.__str__())
-                    logging.exception(e)
 
     def parse_page(self, response):
         page_count = int(self.parse_pagenum(response))
         try:
             for pagenum in range(page_count):
-                if pagenum > 0:
-                    yield SplashRequest(response.meta['url'],
-                        endpoint='execute',
-                        args={
-                            'lua_source': script,
-                            'wait': 1,
-                            'page': pagenum,
-                            'url': response.meta['url'],
-                        },
-                        callback=self.parse,
-                        meta=response.meta)
+                yield SplashRequest(url,
+                                    endpoint='execute',
+                                    args={
+                                        'lua_source': script,
+                                        'wait': 1,
+                                        'page': pagenum + 1,
+                                        'url': url,
+                                    },
+                                    callback=self.parse,
+                                    dont_filter=True)
         except Exception as e:
             logging.error(self.name + ": " + e.__str__())
             logging.exception(e)
+
     def parse_pagenum(self, response):
         try:
             # 在解析页码的方法中判断是否增量爬取并设定爬取列表页数，如果运行
             # 脚本时没有传入参数pagenum指定爬取前几页列表页，则全量爬取
             if not self.add_pagenum:
-                return int(response.xpath('//*[@id="MoreInfoList1_Pager"]//b/text()').extract()[1])
+                return int(response.css(
+                    '#MoreInfoList1_Pager > table > tbody > tr > td:nth-child(1) > font:nth-child(2) > b::text').extract_first())
             return self.add_pagenum
         except Exception as e:
             logging.error(self.name + ": " + e.__str__())
             logging.exception(e)
 
     def parse(self, response):
-        for selector in response.xpath('//*[@class="DataGrid"]/tbody/tr'):
+        for selector in response.css('.DataGrid tr'):
             try:
-                if selector.xpath('./td[2]/a/text()'):
+                if selector.css('td:nth-child(2) a::text'):
                     item = {}
-                    item['title'] = selector.xpath('./td[2]/a/text()').extract_first()
-                    item['time'] = selector.xpath('./td[3]/text()').extract_first()
-                    url = 'http://gxggzy.gxzf.gov.cn' + selector.xpath('./td[2]/a/@href').extract_first()
-                    item['url'] = url
-                    yield scrapy.Request(url,callback=self.parse_item, dont_filter=True, meta=item)
+                    item['title'] = selector.css('td:nth-child(2) a::attr(title)').extract_first()
+                    item['time'] = selector.css('td:nth-child(3)::text').extract_first().strip()
+                    item['url'] = response.urljoin(selector.css('td:nth-child(2) a::attr(href)').extract_first())
+                    yield scrapy.Request(url, callback=self.parse_item, dont_filter=True, meta=item)
             except Exception as e:
                 logging.error(self.name + ": " + e.__str__())
                 logging.exception(e)
