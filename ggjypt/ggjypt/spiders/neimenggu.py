6e302ec91b9c83756fd3851d8099732dd9748ecf
sn_baby <sn_baby@qq.com>
2020-09-12 22:13:55 +0800

    修改

--- ggjypt/ggjypt/spiders/neimenggu.py
+++ ggjypt/ggjypt/spiders/neimenggu.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
 import scrapy
 import logging
+import json
 
 from scrapy_splash import SplashRequest
 from ggjypt.items import ztbkItem
-from utils.tools.attachment import get_attachments,get_times
+from utils.tools.attachment import get_attachments, get_times
 import time
+
 script = """
 function wait_for_element(splash, css, maxwait)
   -- Wait until a selector matches an element
@@ -48,23 +50,24 @@ function main(splash, args)
 end
 """
 
+
 class GansuSpider(scrapy.Spider):
     name = 'neimenggu_ggjypt'
     custom_settings = {
         'CONCURRENT_REQUESTS': 10,
         'CONCURRENT_REQUESTS_PER_DOMAIN': 10,
         'CONCURRENT_REQUESTS_PER_IP': 0,
-        'DOWNLOAD_DELAY': 0.5,
-        'DOWNLOADER_MIDDLEWARES' : {
+        'DOWNLOAD_DELAY': 2,
+        'DOWNLOADER_MIDDLEWARES': {
             'scrapy_splash.SplashCookiesMiddleware': 723,
             'scrapy_splash.SplashMiddleware': 725,
             'scrapy.downloadermiddlewares.httpcompression.HttpCompressionMiddleware': 810,
         },
-        'SPIDER_MIDDLEWARES' : {
+        'SPIDER_MIDDLEWARES': {
             'scrapy_splash.SplashDeduplicateArgsMiddleware': 100,
         },
         'DUPEFILTER_CLASS': 'scrapy_splash.SplashAwareDupeFilter',
-        'HTTPCACHE_STORAGE' : 'scrapy_splash.SplashAwareFSCacheStorage',
+        'HTTPCACHE_STORAGE': 'scrapy_splash.SplashAwareFSCacheStorage',
         # 'SPIDER_MIDDLEWARES': {
         #     'scrapy_splash.SplashDeduplicateArgsMiddleware': 100,
         # },
@@ -79,7 +82,7 @@ class GansuSpider(scrapy.Spider):
         },
         # 'DUPEFILTER_CLASS': 'scrapy_splash.SplashAwareDupeFilter',
         # 'HTTPCACHE_STORAGE': 'scrapy_splash.SplashAwareFSCacheStorage',
-        'SPLASH_URL': "http://localhost:8050/"}
+        'SPLASH_URL': "http://47.57.108.128:8050/"}
 
     def __init__(self, pagenum=None, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -132,15 +135,9 @@ class GansuSpider(scrapy.Spider):
                 }
             ]
             for content in contents:
-                yield SplashRequest(content['url'],
-                                    endpoint='execute',
-                                    args={
-                                        'lua_source': script,
-                                        'page': 1,
-                                        'url': content['url'],
-                                    },
-                                    callback=self.parse_page,
-                                    meta=content)
+                yield scrapy.Request(content['url'], headers={
+                    'User-Agent': 'Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.93 Safari/537.36'},
+                                     callback=self.parse_page, dont_filter=True, meta=content)
         except Exception as e:
             logging.error(self.name + ": " + e.__str__())
             logging.exception(e)
@@ -149,40 +146,47 @@ class GansuSpider(scrapy.Spider):
         page_count = int(self.parse_pagenum(response))
         try:
             for pagenum in range(page_count):
-                if pagenum > 0:
-                    yield SplashRequest(response.meta['url'],
-                                        endpoint='execute',
-                                        args={
-                                            'lua_source': script,
-                                            'page': pagenum,
-                                            'url': response.meta['url'],
-                                        },
-                                        callback=self.parse,
-                                        meta=response.meta)
+                data = {
+                    'currentPage': str(pagenum + 1),
+                    'industriesTypeCode': '000'
+                }
+                yield scrapy.FormRequest(url=response.meta['url'], formdata=data, method='POST',
+                                     headers={
+                                         'User-Agent': 'Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.93 Safari/537.36',
+                                         'Accept': '*/*'
+                                     },
+                                     dont_filter=True,
+                                     callback=self.parse)
         except Exception as e:
             logging.error(self.name + ": " + e.__str__())
             logging.exception(e)
+
     def parse_pagenum(self, response):
         try:
             # 在解析页码的方法中判断是否增量爬取并设定爬取列表页数，如果运行
             # 脚本时没有传入参数pagenum指定爬取前几页列表页，则全量爬取
             if not self.add_pagenum:
-                return int(response.xpath('//*[@class="mmggxlh"]/a[last()-1]/text()').extract()[0]) + 1
+                return int(response.xpath('//*[@class="mmggxlh"]/a[last()-1]/text()').extract()[0])
             return self.add_pagenum
         except Exception as e:
             logging.error(self.name + ": " + e.__str__())
             logging.exception(e)
+
     def parse(self, response):
-        for selector in response.xpath('//*[@class="content_right fr"]/div[4]/table/tbody/tr'):
-            try:
-                item = {}
-                item['title'] = selector.xpath('./td[3]/a/text()').extract_first()
-                item['time'] = selector.xpath('./td[4]/text()').extract_first()
-                url = response.urljoin(selector.xpath('./td[3]/a/@href').extract_first())
-                yield scrapy.Request(url,callback=self.parse_item, dont_filter=True, meta=item)
-            except Exception as e:
-                logging.error(self.name + ": " + e.__str__())
-                logging.exception(e)
+        for selector in response.css('table tr'):
+            if selector.css('td:nth-child(3) a'):
+                try:
+                    item = {}
+                    item['title'] = selector.css('td:nth-child(3) a::text').extract_first().strip()
+                    item['time'] = selector.css('td:nth-child(4)::text').extract_first()
+                    url = response.urljoin(selector.css('td:nth-child(3) a::attr(href)').extract_first())
+                    yield scrapy.Request(url, callback=self.parse_item, headers={
+                        'User-Agent': 'Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.93 Safari/537.36',
+                        'Accept': '*/*'
+                    }, dont_filter=True, meta=item)
+                except Exception as e:
+                    logging.error(self.name + ": " + e.__str__())
+                    logging.exception(e)
 
     def parse_item(self, response):
         if response.meta['title']:
