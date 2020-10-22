269201e93a9286bab1c77e66cab156f4d2bab5b7
sn_baby <sn_baby@qq.com>
2020-10-23 01:12:07 +0800

    修改

--- zfcgw/zfcgw/spiders/shanxi_zfcgw.py
+++ zfcgw/zfcgw/spiders/shanxi_zfcgw.py
@@ -4,7 +4,8 @@ import logging
 
 from scrapy_splash import SplashRequest
 from zfcgw.items import ztbkItem
-from utils.tools.attachment import get_attachments,get_times
+from utils.tools.attachment import get_attachments, get_times
+
 
 class ShanxiZfcgwSpider(scrapy.Spider):
     name = 'shanxi_zfcgw'
@@ -26,201 +27,113 @@ class ShanxiZfcgwSpider(scrapy.Spider):
             'utils.pipelines.DuplicatesPipeline.DuplicatesPipeline': 100,
         },
         'DUPEFILTER_CLASS': 'scrapy_splash.SplashAwareDupeFilter'
-        }
+    }
 
     def __init__(self, pagenum=None, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.add_pagenum = pagenum
 
     def start_requests(self):
-        script = """
-        function wait_for_element(splash, css, maxwait)
-          -- Wait until a selector matches an element
-          -- in the page. Return an error if waited more
-          -- than maxwait seconds.
-          if maxwait == nil then
-              maxwait = 10
-          end
-          return splash:wait_for_resume(string.format([[
-            function main(splash) {
-              var selector = '%s';
-              var maxwait = %s;
-              var end = Date.now() + maxwait*1000;
-
-              function check() {
-                if(document.querySelector(selector)) {
-                  splash.resume('Element found');
-                } else if(Date.now() >= end) {
-                  var err = 'Timeout waiting for element';
-                  splash.error(err + " " + selector);
-                } else {
-                  setTimeout(check, 200);
-                }
-              }
-              check();
-            }
-          ]], css, maxwait))
-        end
-        function main(splash, args)
-          splash:go(args.url)
-          wait_for_element(splash, ".list-box td[title]")
-          splash:runjs("document.querySelector('.list-box').innerHTML = ''")
-          js = string.format("regionCity('%s','%s');totype('%s');", args.code,args.name,args.type)
-          splash:evaljs(js)
-          wait_for_element(splash, ".list-box td[title]")
-          splash:wait(1)
-          return splash:html()
-        end
-        """
         try:
-            contents = [
+            regions = [
                 {
-                    'topic': 'cggg',  # 采购公告
-                    'url': 'http://www.ccgp-shaanxi.gov.cn/notice/list.do?noticetype=3&province='
+                    'code': '610001',
+                    'name': '陕西省本级'
+                },
+                {
+                    'code': '6101',
+                    'name': '西安市'
+                },
+                {
+                    'code': '6102',
+                    'name': '铜川市'
+                },
+                {
+                    'code': '6103',
+                    'name': '宝鸡市'
+                },
+                {
+                    'code': '6104',
+                    'name': '咸阳市'
+                },
+                {
+                    'code': '6105',
+                    'name': '渭南市'
+                },
+                {
+                    'code': '6106',
+                    'name': '延安市'
+                },
+                {
+                    'code': '6107',
+                    'name': '汉中市'
+                },
+                {
+                    'code': '6108',
+                    'name': '榆林市'
+                },
+                {
+                    'code': '6109',
+                    'name': '安康市'
+                },
+                {
+                    'code': '6110',
+                    'name': '商洛市'
+                },
+                {
+                    'code': '6111',
+                    'name': '杨凌示范区'
+                },
+                {
+                    'code': '6169',
+                    'name': '西咸新区'
                 }
             ]
-            for content in contents:
-                regions = [
-                    {
-                        'code': '610001',
-                        'name': '陕西省本级'
-                    },
-                    {
-                        'code': '6101',
-                        'name': '西安市'
-                    },
-                    {
-                        'code': '6102',
-                        'name': '铜川市'
-                    },
-                    {
-                        'code': '6103',
-                        'name': '宝鸡市'
-                    },
-                    {
-                        'code': '6104',
-                        'name': '咸阳市'
-                    },
-                    {
-                        'code': '6105',
-                        'name': '渭南市'
-                    },
-                    {
-                        'code': '6106',
-                        'name': '延安市'
-                    },
-                    {
-                        'code': '6107',
-                        'name': '汉中市'
-                    },
-                    {
-                        'code': '6108',
-                        'name': '榆林市'
-                    },
-                    {
-                        'code': '6109',
-                        'name': '安康市'
-                    },
-                    {
-                        'code': '6110',
-                        'name': '商洛市'
-                    },
-                    {
-                        'code': '6111',
-                        'name': '杨凌示范区'
-                    },
-                    {
-                        'code': '6169',
-                        'name': '西咸新区'
+            types = [
+                '3', '5', '4', '6', '99', '1'
+            ]
+            for region in regions:
+                for type in types:
+                    url = 'http://www.ccgp-shaanxi.gov.cn/notice/noticeaframe.do?isgovertment=&noticetype=' + type
+                    data = {
+                        "parameters['purcatalogguid']": "",
+                        "page.pageNum": "1",
+                        "parameters['title']": "",
+                        "parameters['startdate']": "",
+                        "parameters['enddate']": "",
+                        "parameters['regionguid']": region['code'],
+                        "parameters['projectcode']": "",
+                        "province": ""
+
                     }
-                ]
-                types = [
-                    '3', '5', '4', '6', '99', '1'
-                ]
-                for region in regions:
-                    for type in types:
-                        yield SplashRequest(content['url'],
-                                            endpoint='execute',
-                                            args={
-                                                'lua_source': script,
-                                                'wait': 1,
-                                                'code': region['code'],
-                                                'name': region['name'],
-                                                'type': type,
-                                                'url': content['url'],
-                                            },
-                                            callback=self.parse_page,
-                                            meta={
-                                                'code': region['code'],
-                                                'name': region['name'],
-                                                'type': type,
-                                                'url': content['url']
-                                            })
+                    yield scrapy.FormRequest(url=url,
+                                             meta={
+                                                 'url': url,
+                                                 'code': region['code']
+                                             },
+                                             formdata=data, method='POST',dont_filter=True, callback=self.parse_page)
         except Exception as e:
             logging.error(self.name + ": " + e.__str__())
             logging.exception(e)
 
     def parse_page(self, response):
-        script = """
-        function wait_for_element(splash, css, maxwait)
-          -- Wait until a selector matches an element
-          -- in the page. Return an error if waited more
-          -- than maxwait seconds.
-          if maxwait == nil then
-              maxwait = 10
-          end
-          return splash:wait_for_resume(string.format([[
-            function main(splash) {
-              var selector = '%s';
-              var maxwait = %s;
-              var end = Date.now() + maxwait*1000;
-
-              function check() {
-                if(document.querySelector(selector)) {
-                  splash.resume('Element found');
-                } else if(Date.now() >= end) {
-                  var err = 'Timeout waiting for element';
-                  splash.error(err + " " + selector);
-                } else {
-                  setTimeout(check, 200);
-                }
-              }
-              check();
-            }
-          ]], css, maxwait))
-        end
-        function main(splash, args)
-          splash:go(args.url)
-          wait_for_element(splash, ".list-box td[title]")
-          splash:runjs("document.querySelector('.list-box').innerHTML = ''")
-          js = string.format("regionCity('%s','%s');totype('%s');", args.code,args.name,args.type)
-          splash:evaljs(js)
-          wait_for_element(splash, ".list-box td[title]")
-          splash:runjs("document.querySelector('.list-box').innerHTML = ''")
-          js = string.format("toPage('',%d);", args.pagenum)
-          splash:evaljs(js)
-          wait_for_element(splash, ".list-box td[title]")
-          splash:wait(1)
-          return splash:html()
-        end
-        """
         page_count = int(self.parse_pagenum(response))
         try:
             for pagenum in range(page_count):
                 url = response.meta['url']
-                yield SplashRequest(url,
-                                    endpoint='execute',
-                                    args={
-                                        'lua_source': script,
-                                        'wait': 1,
-                                        'pagenum': pagenum + 1,
-                                        'url': url,
-                                        'code': response.meta['code'],
-                                        'name': response.meta['name'],
-                                        'type': response.meta['type']
-                                    },
-                                    callback=self.parse,
-                                    meta=response.meta)
+                data = {
+                    "parameters['purcatalogguid']": "",
+                    "page.pageNum": str(pagenum+1),
+                    "parameters['title']": "",
+                    "parameters['startdate']": "",
+                    "parameters['enddate']": "",
+                    "parameters['regionguid']": response.meta['code'],
+                    "parameters['projectcode']": "",
+                    "province": ""
+
+                }
+                yield scrapy.FormRequest(url=url,
+                                         formdata=data, method='POST', dont_filter=True, callback=self.parse)
         except Exception as e:
             logging.error(self.name + ": " + e.__str__())
             logging.exception(e)
@@ -230,7 +143,7 @@ class ShanxiZfcgwSpider(scrapy.Spider):
             # 在解析页码的方法中判断是否增量爬取并设定爬取列表页数，如果运行
             # 脚本时没有传入参数pagenum指定爬取前几页列表页，则全量爬取
             if not self.add_pagenum:
-                return int(response.css('#dateTable > div > div > ul > li:nth-last-child(3) > a::text').extract_first().strip())
+                return int(response.css('.pagination li:nth-last-child(5) a::text').extract_first().strip())
             return self.add_pagenum
         except Exception as e:
             logging.error(self.name + ": " + e.__str__())
@@ -241,14 +154,13 @@ class ShanxiZfcgwSpider(scrapy.Spider):
             try:
                 title = tr.css('td[title]::attr(title)').extract_first()
                 url = response.urljoin(tr.css('td[title] a::attr(href)').extract_first())
-                region = tr.css('td:nth-child(2)::text').extract_first().replace('[','').replace(']','')
+                region = tr.css('td:nth-child(2)::text').extract_first().replace('[', '').replace(']', '')
                 time = tr.css('td:nth-child(4)::text').extract_first()
                 result = {
                     'url': url,
                     'title': title,
                     'region': region,
                     'time': time
-
                 }
                 yield scrapy.Request(url, callback=self.parse_item, meta=result, dont_filter=True)
             except Exception as e:
